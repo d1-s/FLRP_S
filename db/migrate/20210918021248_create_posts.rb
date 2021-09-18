35db44afaec2753d11969dd237c0ea85eb26bdec
d1-s <d.sub.second.01@gmail.com>
2021-09-18 11:25:15 +0900

    postsテーブル作成

--- db/migrate/20210918021248_create_posts.rb
+++ db/migrate/20210918021248_create_posts.rb
@@ -1,7 +1,15 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.date :visit,         null: false
+      t.string :restaurant,  null: false
+      t.integer :category,   null: false
+      t.time :open,          null: false
+      t.time :close,         null: false
+      t.string :address,     null: false
+      t.integer :budget,     null: false
+      t.text :introduction
+      t.references :user,    null: false, foreign_key: true
       t.timestamps
     end
   end
