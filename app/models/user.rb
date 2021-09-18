d837c89b19a1024dfea88614a85691cdb01ccb76
d1-s <d.sub.second.01@gmail.com>
2021-09-18 11:22:34 +0900

    Userモデルとのアソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :posts
+
   enum sex: { man: 0, woman: 1}
   with_options presence: true do
     validates :name
