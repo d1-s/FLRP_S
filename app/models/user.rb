3bd8d6f0eef1161ce4d96f16ae5bdbccf7fe3b2e
d1-s <d.sub.second.01@gmail.com>
2021-09-20 14:21:05 +0900

    アソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  has_many :comments
 
   enum sex: { man: 0, woman: 1}
   with_options presence: true do
