7f2a4223e33afbc22d2df882b4835aab1825a6f1
d1-s <d.sub.second.01@gmail.com>
2021-09-24 21:59:57 +0900

    サイドバー固定に伴い、トップページを修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,6 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-// require("../search")
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -18,3 +16,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// = require jquery3
+// = require popper
+// = require bootstrap-sprockets
+// = require data-confirm-modal
