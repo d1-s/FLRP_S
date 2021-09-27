37b5de589c5f98a5e3bd28b5caa2d491abb6fe4c
d1-s <d.sub.second.01@gmail.com>
2021-09-27 21:46:53 +0900

    ユーザー情報編集ページの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :gender, :profile])
   end
 end
